682f8ba3cebd8d3a9e1a51cd40110c41a41aef33
Jimmy Lam <jimmy6860@gmail.com>
2020-04-23 16:46:44 +0200

    I created the landingpage and add a new folder call style

---
+++ src/style/theme/MainTheme.jsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#E53935'
+        },
+    }
+})
+
+export default theme
