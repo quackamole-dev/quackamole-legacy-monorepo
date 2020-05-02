b9e2e5e33ff9f6772a1736809c9978a787cbcb63
Galvandi <andreas_schoch@outlook.com>
2020-05-02 22:42:22 +0200

    Updates

---
+++ precache-manifest.577c170bab6561372098bf9922a99f87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f88cbe0106e8e8c6d60c18570df649a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ea78a111a04c2c11b12",
+    "url": "./static/js/2.8aacaae1.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "./static/js/2.8aacaae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecaef75e4f2671f6a26a",
+    "url": "./static/js/main.8eeaa0fa.chunk.js"
+  },
+  {
+    "revision": "ed8b1eaf57cade747198",
+    "url": "./static/js/runtime-main.0b1b1b5a.js"
+  }
+]);
