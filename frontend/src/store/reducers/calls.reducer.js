42e3f44d3b88fa17106ee901987a421822c29a3f
Galvandi <andreas_schoch@outlook.com>
2020-06-27 18:08:38 +0200

    refactored calls.reducer using immerjs

--- frontend/src/store/reducers/calls.reducer.js
+++ frontend/src/store/reducers/calls.reducer.js
@@ -1,30 +1,31 @@
 import {ADD_CALL, REMOVE_CALL, SET_CALLS_ERROR} from '../actionTypes';
+import produce from 'immer'
 
 const initialState = {
     data: {},
     error: null
 };
 
-const callsReducer = (calls = initialState, action) => {
+const callsReducer = produce((callsDraft, action) => {
     switch (action.type) {
         case ADD_CALL: {
             const call = action.payload.call;
-            return {data: {...calls.data, [call.peer]: call}, error: null};
+            callsDraft.data[call.peer] = call;
+            callsDraft.error = null;
+            return;
         }
         case REMOVE_CALL: {
-            const newData = {...calls.data};
             const call = action.payload.call;
-            delete newData[call.peer];
-            return {data: newData, error: null};
+            delete callsDraft.data[call.peer];
+            callsDraft.error = null;
+            return;
         }
         case SET_CALLS_ERROR: {
-            return {...calls, error: action.payload.error};
+            callsDraft.error = null;
+            return;
         }
-        default: {
-            return calls;
-        }
+        default: {}
     }
-};
+}, initialState);
 
 export default callsReducer;
-
